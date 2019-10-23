61d55d7df059ee26537b281c4f90d5aad6fb8b71
Elad Katz <eladkt@gmail.com>
2019-10-23 18:01:03 +0300

    put chrome-finder dependency in and made it not look for canary or chromium versions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const findChrome = require('chrome-finder');
+const findChrome = require('./chrome-finder');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const path = require('path');
